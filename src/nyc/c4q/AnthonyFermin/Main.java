f14e894521378ac9481bd099cfce7dcf8e8005a6
AnthonyFermin <AnthonyFerminAFC@gmail.com>
2015-03-15 11:30:35 -0400

    initial commit

---
+++ src/nyc/c4q/AnthonyFermin/Main.java
@@ -0,0 +1,11 @@
+package nyc.c4q.AnthonyFermin;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String s = "Hello, world!";
+        System.out.println(s.length());
+
+
+    }
+}
